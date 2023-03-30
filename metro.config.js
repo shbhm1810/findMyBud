46c071cff665874602d10e6347587021797406cf
shbhm1810 <shubhamsharma81327@gmail.com>
2023-03-31 00:06:01 +0530

    added react-native svg

--- metro.config.js
+++ metro.config.js
@@ -4,14 +4,20 @@
  *
  * @format
  */
-
+const defaultConfig = require('metro-config/src/defaults/defaults');
+const {sourceExts, assetExts} = defaultConfig;
 module.exports = {
   transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
         inlineRequires: true,
       },
     }),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
   },
 };

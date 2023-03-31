c6e5a82d943f77c03823fbc45ff330e984757a52
shbhm1810 <shubhamsharma81327@gmail.com>
2023-03-31 23:18:45 +0530

    added fonts and pallets

--- src/navigation/OnboardingNavigator.tsx
+++ src/navigation/OnboardingNavigator.tsx
@@ -1,35 +1,23 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from '../screens/login-screen/LoginScreen';
 
 const Stack = createNativeStackNavigator();
-function SignIn() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>SignIn</Text>
-    </View>
-  );
-}
-function SignOut() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>SignOut</Text>
-    </View>
-  );
-}
+
 function OnBoardingNavigator(): JSX.Element {
   return (
     <Stack.Navigator>
       <Stack.Screen
         name="SignIn"
-        component={SignIn}
+        component={LoginScreen}
         options={{headerShown: false}}
       />
-      <Stack.Screen
+      {/* <Stack.Screen
         name="SignOut"
         component={SignOut}
         options={{headerShown: false}}
-      />
+      /> */}
     </Stack.Navigator>
   );
 }
